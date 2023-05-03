e1fffa04667664879f772e83b6ad50fbdd095a4c
Bayron David <bayron.david.morales@gmail.com>
2023-05-03 05:35:30 -0500

    Orbit controls

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -12,17 +12,29 @@ public class CameraOrbit : MonoBehaviour
     public float angle;
     private float damping = 3.0f;
     private Quaternion rotation;
+    private ObjectManipulation objectManipulation;
+
+    private void Start()
+    {
+        if (objectManipulation == null)
+        {
+            objectManipulation = GetComponent<ObjectManipulation>();
+        }
+    }
 
 
     private void LateUpdate(){
         if(nodo.activeSelf && panelInformacion.enabled == true){
             transform.position = nodo.transform.position + new Vector3 (distance[0],distance[1],distance[2]); //3, 0 , 1.5
             rotation = Quaternion.Euler (0,angle,0);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);     
-        }else if(panelInformacion.enabled == false){
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+            objectManipulation.enabled = true;
+        }
+        else if(panelInformacion.enabled == false){
             transform.position = new Vector3 (0,0,cameraMain.transform.position.z);
             rotation = Quaternion.Euler (0.0f,0.0f,0.0f);
             transform.rotation = rotation;
+            objectManipulation.enabled = false;
             cameraOrbit.enabled = false;
             cameraMain.enabled = true;
         }
